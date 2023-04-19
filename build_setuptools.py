0265af0d889f9bef9b9a6c587a55c7cf8e4f1248
Scott Chase Waggener <tidalpaladin@protonmail.com>
2023-04-19 15:49:31 -0500

    Fix issues with linking after install (#10)
    * Fix issues with linking after install
    
    * Rework resource freeing

--- build_setuptools.py
+++ build_setuptools.py
@@ -54,6 +54,8 @@ class CMakeBuild(build_ext):
             f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}{os.sep}",
             f"-DPYTHON_EXECUTABLE={sys.executable}",
             f"-DCMAKE_BUILD_TYPE={cfg}",  # not used on MSVC, but no harm
+            "-DCMAKE_BUILD_WITH_INSTALL_RPATH=TRUE",
+            "-DCMAKE_INSTALL_RPATH=$ORIGIN",
         ]
         build_args = []
         # Adding CMake arguments set as environment variable
